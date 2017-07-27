9cf5b023f242076c8a4a849214734be2a58ba4ef
lc-mkimura <kimura_m@lightcafe.co.jp>
2017-07-27 11:18:04 +0900

    _commonディレクトリ内のJSをconcatして出力

--- gulp/script.js
+++ gulp/script.js
@@ -1,38 +1,54 @@
 /**
  * スクリプトタスク
- * JSファイルをwebpackを使ってコンパイルして出力する
+ * JSファイルをコンパイルして出力する
  */
 var gulp = require('gulp'),
-    // path = require('path'),
-    webpack = require("gulp-webpack");
-    // named = require('vinyl-named');
+    plumber = require('gulp-plumber'),
+    concat = require('gulp-concat'),
+    named = require('vinyl-named');
 var conf = require('../webpack.config.js');
 
 /**
- * webpackコンパイル開始
+ * コンパイル開始
  * @param watch
  * @returns {*}
  */
 function exeWebPack(watch) {
   conf.watch = watch;
+  return gulp.src(__CONFIG.path.jsconcat.src)
+    .pipe(named())
+    .pipe(plumber())
+    .pipe(concat(__CONFIG.path.jsconcat.filename))
+    //.pipe($.uglify()) //圧縮します
+    .pipe(gulp.dest(__CONFIG.path.jsconcat.dest))
+    .pipe($.browser.stream());
+}
+
+function exeScript(watch) {
+  conf.watch = watch;
   return gulp.src(__CONFIG.path.js.src)
-    // .pipe(named())
-    // .pipe(webpack(conf))
-    // .pipe($.uglify()) //圧縮します
+    .pipe(named())
+    //.pipe($.uglify()) //圧縮します
     .pipe(gulp.dest(__CONFIG.path.js.dest))
     .pipe($.browser.stream());
+}
+
+function taskScript(watch) {
+  exeWebPack(watch);
+  exeScript(watch);
+  return;
 }
 
 /**
  * スクリプトコンパイルタスク
  */
 gulp.task('script', function() {
-  return exeWebPack(false);
+  return taskScript(false);
 });
 
 /**
  * スクリプト監視タスク
  */
 gulp.task('watchScript', function() {
-  return exeWebPack(true);
+  return taskScript(true);
 });
